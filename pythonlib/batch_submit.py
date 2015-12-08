dd4a700ec85b1999c21fba95c509a09fe213cb04
rbayes <ryan.bayes@glasgow.ac.uk>
2015-12-08 14:02:59 +0000

    Pre pull commit

--- pythonlib/batch_submit.py
+++ pythonlib/batch_submit.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from time import sleep
 
 class batch_submit:
 
@@ -7,7 +8,8 @@ class batch_submit:
 		self.seed = 1
 		self.home = os.getenv("HOME")
 		self.working_dir = os.path.join(self.home,'SaRoMaN')
-		self.instances = 100
+		self.out_dir = os.path.join(self.home,'batch2')
+		self.instances = 20
 		#self.configfile =
 		self.batchfile = self.working_dir + "/run_"+str(self.seed)+".job"
 
@@ -22,10 +24,10 @@ class batch_submit:
 #PBS -N saroman.py_%(seed)d
 #PBS -q medium6
 #PBS -l walltime=5:59:00
-#PBS -e %(working_dir)s/saroman.py_%(seed)d.err
-#PBS -o %(working_dir)s/saroman.py_%(seed)d.out
+#PBS -e %(out_dir)s/saroman.py_%(seed)d.err
+#PBS -o %(out_dir)s/saroman.py_%(seed)d.out
 
-python %(working_dir)s/saroman.py 
+python %(working_dir)s/saroman.py -B %(seed)d
 '''% vars(self)
 
 		#--configuration_file %(configfile)s
@@ -34,15 +36,23 @@ python %(working_dir)s/saroman.py
         
 	def generate_submission(self):
         
-		for self.seed in range(1,self.instances):
+		for self.seed in range(0,self.instances):
 			#self.configfile = self.working_dir + "/maus_config_"+str(self.seed)
 			#self.print_config()
 			self.print_batch_submission()
 			cmd = ['qsub', self.batchfile]
 			q = subprocess.Popen(cmd)
+			sleep(3) #Wait 3 sec to make sure that all processes are submitted correctly.
 
 
 if __name__ == "__main__":
+	b = batch_submit()
+	b.generate_submission()
 
-    b = batch_submit()
-    b.generate_submit()
+	#REMOVE JOBS FROM QSTAT
+	#for x in range(1795878,1796077):
+		#string = str(x) + '.offler'
+		#cmd = ['qdel', string]
+		#subprocess.Popen(cmd)
+		#sleep(0.5)
+	
